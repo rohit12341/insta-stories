6df2ddd919d3391ffff8d7653d06e3d904b04361
rohit12341 <30390511+rohit12341@users.noreply.github.com>
2024-08-10 02:34:47 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-96 mx-auto border border-gray-300">
-      hi
+    <main className="min-h-screen w-96 mx-auto flex items-center justify-center border bg-black border-gray-300">
+      <Link className="px-4 py-2 rounded-md bg-black border border-white text-white shadow-gray" href="/stories">
+        Go to Stories
+      </Link>
     </main>
   );
 }
